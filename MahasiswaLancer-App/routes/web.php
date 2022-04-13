1aec1795a16252a218090519c16ef7ac9fe93d30
DarmaP135 <darma.wijaya1010@gmail.com>
2022-04-13 18:43:32 +0700

    [Done: File BiodataMahasiswa resolve #6]

--- MahasiswaLancer-App/routes/web.php
+++ MahasiswaLancer-App/routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+/*Mahasiswa*/
+Route::get('/biodata', [App\Http\Controllers\BiodataMHSController::class, 'index'])->name('biodata'); 
