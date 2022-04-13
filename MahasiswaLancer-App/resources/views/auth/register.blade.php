f9894805449ef13c9a9844ceb30ff1435cb8a3e1
Klaudius Waditra Pradnya <73590144+klaudiuswp@users.noreply.github.com>
2022-04-13 22:45:01 +0700

    update backend

--- MahasiswaLancer-App/resources/views/auth/register.blade.php
+++ MahasiswaLancer-App/resources/views/auth/register.blade.php
@@ -6,26 +6,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="login-regis-images/icons/favicon.ico" />
+    <link rel="icon" type="image/png" href="asset-images/icons/favicon.ico" />
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="login-regis-vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="asset-vendor/bootstrap/css/bootstrap.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="login-regis-fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="asset-fonts/font-awesome-4.7.0/css/font-awesome.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="login-regis-fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <link rel="stylesheet" type="text/css" href="asset-fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="login-regis-vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="asset-vendor/animate/animate.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="login-regis-vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="asset-vendor/css-hamburgers/hamburgers.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="login-regis-vendor/animsition/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css" href="asset-vendor/animsition/css/animsition.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="login-regis-vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="asset-vendor/select2/select2.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="login-regis-vendor/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" type="text/css" href="asset-vendor/daterangepicker/daterangepicker.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="css/login-regis-util.css">
-    <link rel="stylesheet" type="text/css" href="css/login-regis-main.css">
+    <link rel="stylesheet" type="text/css" href="css/asset-util.css">
+    <link rel="stylesheet" type="text/css" href="css/asset-main.css">
     <!--===============================================================================================-->
 </head>
 
@@ -107,8 +107,8 @@
 
                 <div class="login100-more">
                     <div class="container">
-                        <p class="p-t-80 text-center"><a href="/"><img src="login-regis-images/LOGO MAHASISWALANCER.png"></a></p>
-                        <p class="p-t-90 text-center"><img src="login-regis-images/ilustrasi.png" style="width: 400px;"></p>
+                        <p class="p-t-80 text-center"><a href="/"><img src="asset-images/LOGO MAHASISWALANCER.png"></a></p>
+                        <p class="p-t-90 text-center"><img src="asset-images/ilustrasi.png" style="width: 400px;"></p>
                         <div class="text-center p-t-30 p-b-10"><b>Selesaikan tugas mu melalui MahasiswaLancer 👋</b>
                         </div>
                         <p class="text-center" style="">Bantu kamu mengatur jadwal kegiatanmu sehari-hari dengan
@@ -125,21 +125,21 @@
 
 
     <!--===============================================================================================-->
-    <script src="login-regis-vendor/jquery/jquery-3.2.1.min.js"></script>
+    <script src="asset-vendor/jquery/jquery-3.2.1.min.js"></script>
     <!--===============================================================================================-->
-    <script src="login-regis-vendor/animsition/js/animsition.min.js"></script>
+    <script src="asset-vendor/animsition/js/animsition.min.js"></script>
     <!--===============================================================================================-->
-    <script src="login-regis-vendor/bootstrap/js/popper.js"></script>
-    <script src="login-regis-vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="asset-vendor/bootstrap/js/popper.js"></script>
+    <script src="asset-vendor/bootstrap/js/bootstrap.min.js"></script>
     <!--===============================================================================================-->
-    <script src="login-regis-vendor/select2/select2.min.js"></script>
+    <script src="asset-vendor/select2/select2.min.js"></script>
     <!--===============================================================================================-->
-    <script src="login-regis-vendor/daterangepicker/moment.min.js"></script>
-    <script src="login-regis-vendor/daterangepicker/daterangepicker.js"></script>
+    <script src="asset-vendor/daterangepicker/moment.min.js"></script>
+    <script src="asset-vendor/daterangepicker/daterangepicker.js"></script>
     <!--===============================================================================================-->
-    <script src="login-regis-vendor/countdowntime/countdowntime.js"></script>
+    <script src="asset-vendor/countdowntime/countdowntime.js"></script>
     <!--===============================================================================================-->
-    <script src="js/login-regis-main.js"></script>
+    <script src="js/asset-main.js"></script>
 
 </body>
 
--- MahasiswaLancer-App/routes/web.php
+++ MahasiswaLancer-App/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('LandingPage');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
